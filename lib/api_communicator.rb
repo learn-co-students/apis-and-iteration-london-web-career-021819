3ea553236a1245fbf9c99c099f7c19868a40748d
github username <github email address>
2019-02-19 14:34:12 +0000

    Answer to starwars api problem

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,39 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def get_character_movies_from_api(character_name)
+  response_string = RestClient.get("http://www.swapi.co/api/people/")
+  response_hash = JSON.parse(response_string)
+  films = nil
+  response_hash["results"].each do |character|
+    if character["name"].downcase == character_name.downcase
+      return films = character["films"]
+    end
+  end
+  if films == nil
+    abort("Invalid character name")
+  end
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+
+def get_movie_names_from_api(films)
+  titles = []
+  films.each do |film|
+    film_response_string = RestClient.get(film)
+    film_response_hash = JSON.parse(film_response_string)
+    titles << film_response_hash["title"]
+  end
+  titles
+end
+
+def print_movies(titles)
+  puts "This character appears in the following films:"
+  titles.each_with_index {|name, index| puts "#{index+1}. #{name}"}
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  titles = get_movie_names_from_api(films)
+  print_movies(titles)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
