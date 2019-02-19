5a8ea5257f0c7439a934c49b8b9b9f02d15bfc66
github username <github email address>
2019-02-19 14:44:21 +0000

    update

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,25 +1,45 @@
+
+require 'pry'
 require 'rest-client'
 require 'json'
-require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  results = response_hash["results"]
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  films = nil
+
+  results.each do |character|
+    if character_name == character["name"]
+      films = character["films"]
+    end
+  end
+
+  if films
+    film_data = films.collect do |film_api|
+      film_response_string = RestClient.get(film_api)
+      film_response_hash = JSON.parse(film_response_string)
+      film_response_hash
+    end
+  else
+    film_data = nil
+  end
+
+  film_data
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  if films
+    films.each_with_index do |film, index|
+      puts "#{index + 1}. #{film["title"]}"
+    end
+  else
+    puts "Do you even like Star Wars?"
+  end
+
 end
 
 def show_character_movies(character)
@@ -27,7 +47,7 @@ def show_character_movies(character)
   print_movies(films)
 end
 
-## BONUS
 
+## BONUS
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
