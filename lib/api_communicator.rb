3c9ed0b9fc072f48df7dff9cb49c13d57266aa68
Alex Bujenita <dekc89@gmail.com>
2019-02-19 15:24:02 +0000

    Error handling in case of no character

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,36 +2,34 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def return_array_films hash_from_json
+    hash_from_json["results"][0]["films"]
+end
+
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get("https://swapi.co/api/people/?search=#{character_name}")
   response_hash = JSON.parse(response_string)
 
-  film_urls = response_hash["results"][0]["films"]
+  # binding.pry
+
+  if response_hash["count"] == 0; return "Err"; end
+
+  film_urls = return_array_films(response_hash)
 
   char_movies = []
-
   film_urls.each_with_index do |url, index|
     resp_hash = JSON.parse(RestClient.get(url))
     char_movies.push(resp_hash["title"])
   end
 
-
-  # binding.pry
-
   char_movies
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
 def print_movies(films)
+  puts "Character not found" if films == "Err"
+  return if films == "Err"
   films.each_with_index { |film, index| puts "#{index+1}: #{film}" }
 end
 
@@ -39,6 +37,7 @@ def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
+
 
 ## BONUS
 
