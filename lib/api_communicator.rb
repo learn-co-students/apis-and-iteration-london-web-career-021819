5796317949f58a39ae24d05cbfaa216022c8249f
github username <github email address>
2019-02-19 14:51:57 +0000

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,8 +4,12 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
+  # puts ("hello")
   response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # puts ("hello")
+
   response_hash = JSON.parse(response_string)
+  # puts ("hello")
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -16,15 +20,39 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  character = response_hash["results"].find do|character| 
+    character["name"].downcase == character_name
+  end
+  if character
+    character['films']
+  else
+    "no films"
+  end
+
 end
 
+def get_film_info(film)
+    film_string = RestClient.get(film)
+    film_hash = JSON.parse(film_string)
+    film_hash["title"]
+end
+
+
 def print_movies(films)
+  films.each do |film_url|
+    title = get_film_info(film_url)
+    puts title
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  if films != "no films"
+    print_movies(films)
+  else
+    puts ("There are no star wars films with that character")
+  end
 end
 
 ## BONUS
