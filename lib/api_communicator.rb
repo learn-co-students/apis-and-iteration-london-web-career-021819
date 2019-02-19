33e2a6cafdc4420d9b8187ced0978bb5c50525c5
github username <github email address>
2019-02-19 17:08:54 +0000

    first version

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,33 +1,44 @@
+require_relative "../lib/command_line_interface.rb"
+
 require 'rest-client'
 require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+response_string = RestClient.get('http://www.swapi.co/api/people/')
+response_hash = JSON.parse(response_string)
+film_titles_array =[]
+  response_hash["results"].each do |element|
+    if element["name"].downcase.include?(character_name)
+      element["films"].each do |filmurl|
+        film_titles_hash = {}
+        response = RestClient.get(filmurl)
+        hash = JSON.parse(response)
+        film_titles_hash[:title] = hash["title"]
+        film_titles_hash[:director] = hash["director"]
+        film_titles_hash[:release] = hash["release_date"]
+        film_titles_array << film_titles_hash
+      end
+    end
+  end
+return film_titles_array
 end
 
+
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |element|
+    puts "*" * 20
+    puts "Title: #{element[:title]}"
+    puts "Director: #{element[:director]}"
+    puts "Release Date: #{element[:release]}"
+  end
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+films = get_character_movies_from_api(character)
+  if films.instance_of? Array
+    puts "Sorry, no character found!"
+  else
+    print_movies(films)
+  end
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
